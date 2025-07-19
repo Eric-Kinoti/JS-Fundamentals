d05e1a6119ba273cad959beac85548327ecfb302
Eric Kinoti <emutuma95@gmail.com>
2025-07-19 17:54:07 +0300

    Fix Task 6: Use for loop and one console.log without if/else

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,3 +1,9 @@
 const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(messages.join('\n'));
+let result = '';
+
+for (let i = 0; i < messages.length; i++) {
+  result += messages[i] + '\n';
+}
+
+console.log(result.trim());
 
